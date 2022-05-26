e999a93c09b16405fb9575d5ad48406b45b5da33
takumi <55521780+tkmyy@users.noreply.github.com>
2022-05-26 20:13:15 +0900

    jsx notation

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
-import { StrictMode } from "react";
-// import { createRoot } from "react-dom/client";
+import React from "react";
 import ReactDom from "react-dom";
 
-import App from "./App";
+const App = () => {
+  return (
+    <>
+      <h1>こんにちは！</h1>
+      <p>お元気ですか？</p>
+    </>
+  );
+};
 
-// const rootElement = document.getElementById("root");
-// const root = createRoot(rootElement);
-
-ReactDom.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-  document.getElementById("root")
-);
+ReactDom.render(<App />, document.getElementById("root"));
