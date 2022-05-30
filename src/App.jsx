e693ee5bb2ab83e5bf8c16170b308aaa7d13a1ef
takumi <55521780+tkmyy@users.noreply.github.com>
2022-05-30 11:36:12 +0900

    limit max todos

--- src/App.jsx
+++ src/App.jsx
@@ -48,7 +48,11 @@ export const App = () => {
         todoText={todoText}
         onChange={onChangeTodoText}
         onClick={onClickAdd}
+        disabled={incompleteTodos.length >= 5}
       />
+      {incompleteTodos.length >= 5 && (
+        <p style={{ color: "red" }}>登録できるTODOは5つまで</p>
+      )}
       <IncompleteTodos
         todos={incompleteTodos}
         onCLickComplete={onCLickComplete}
